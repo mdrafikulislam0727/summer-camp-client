c8c94933e009f49e7f72288a4128da7f96ebcffb
mdrafikulislam <mdrafikulislam0727@gmail.com>
2023-06-07 23:20:32 +0600

    added the login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,55 +1,57 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 
 const Login = () => {
+  const { signIn } = useContext(AuthContext)
 
+  const handelLogin = event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password)
+    signIn(email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user)
+      })
+  }
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content w-full flex-col lg:flex-row-reverse">
+        <div className="text-center w-1/2 lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
 
-    const handelLogin  = event =>{
-        event.preventDefault();
-        const form =event.target;
-        const email =form.email.value;
-        const password =form.password.value;
-        console.log(email, password)
-        // signIn(email, password)
-        // .then(result =>{
-        //     const user =result.user;
-        //     console.log(user)
-        // })
-    }
-    return (
-        <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content w-full flex-col lg:flex-row-reverse">
-          <div className="text-center w-1/2 lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            
-          </div>
-          <div className="card flex-shrink-0 w-1/2 max-w-sm shadow-2xl bg-base-100">
-            <form onSubmit={handelLogin} className="card-body py-10">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="email" name="email" placeholder="email" className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="password"name="password" placeholder="password" className="input input-bordered" />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <input className="btn btn-primary" type="submit" value="Login" />
-              </div>
+        </div>
+        <div className="card flex-shrink-0 w-1/2 max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handelLogin} className="card-body py-10">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="email" name="email" placeholder="email" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="password" name="password" placeholder="password" className="input input-bordered" />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-primary" type="submit" value="Login" />
+            </div>
             <p>New here? <Link to="/signup" className="text-orange-400 font-bold pb-4">Create a New Account</Link></p>
-            </form>
-          </div>
+          </form>
         </div>
       </div>
-    );
- 
+    </div>
+  );
+
 };
 
 export default Login;
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,10 +1,78 @@
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const SignUp = () => {
-    return (
-        <div>
-            <h2>sign up</h2>
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {createUser}=useContext(AuthContext)
+
+  const onSubmit = data => {
+    console.log(data);
+    createUser(data.email, data.password)
+    .then(result =>{
+      const loggedUser = result.user;
+      console.log(loggedUser)
+    })
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content w-full flex-col lg:flex-row-reverse">
+        <div className="text-center w-1/2 lg:text-left">
+          <h1 className="text-5xl font-bold">Sign Up Now</h1>
+
         </div>
-    );
+        <div className="card flex-shrink-0 w-1/2 max-w-sm shadow-2xl bg-base-100 mt-20">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body py-10">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input type="text" {...register("name", { required: true })} name="name" placeholder="Your name" className="input input-bordered" />
+              {errors.name && <span className="text-red-500">Name is required</span>}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="email" {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
+              {errors.email && <span className="text-red-500">Email is required</span>}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo Url</span>
+              </label>
+              <input type="url" name="photo" placeholder="photo" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="password" {...register("password", {
+                required: true,
+                minLength: 6,
+                maxLength: 20,
+                pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+              })} name="password" placeholder="password" className="input input-bordered" />
+
+              {errors.password?.type === 'minLength' && <p className="text-red-600">password must be 6 characters</p>}
+              {errors.password?.type === 'maxLength' && <p className="text-red-600">password must be lessThen 20 characters</p>}
+              {errors.password?.type === 'pattern' && <p className="text-red-600">password must have one upper case one lower case, one number and one spacial character</p>}
+
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-primary" type="submit" value="Sign Up" />
+            </div>
+            <p>Alreday registered ?<Link to="/login" className="text-orange-400 font-bold pb-4">Go to login</Link></p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
