bae8b3d3fb5b007b5d69213ae94871fcfc260c61
mdrafikulislam <mdrafikulislam0727@gmail.com>
2023-06-12 10:42:26 +0600

    add item

--- src/Pages/Dashboard/AddClasses/AddClasses.jsx
+++ src/Pages/Dashboard/AddClasses/AddClasses.jsx
@@ -1,62 +1,97 @@
+import { useForm } from 'react-hook-form';
+import useAuth from '../../../hooks/useAuth';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+
+
+const img_hosting_token = import.meta.env.VITE_Image_upload_token;
+const img_hosting_url =`https://api.imgbb.com/1/upload?key=${img_hosting_token}`
 
 const AddClasses = () => {
+    const [axiosSecure]=useAxiosSecure()
+    const {user}=useAuth()
+    console.log(user)
+    const { register, handleSubmit, } = useForm();
+const onSubmit = data =>{ 
+    const formData = new FormData();
+    formData.append('image', data.image[0])
+    fetch(img_hosting_url, {
+        method:'POST',
+        body:formData
+    })
+    .then(res => res.json())
+    .then(imagResponse =>{
+        if(imagResponse.success){
+            const imgURl=imagResponse.data.display_url;
+            const {name,email,instructorName,price,availableSeats}=data;
+            const newClass = {name, email, instructorName, price, availableSeats, image:imgURl, status: 'pending'}
+            console.log(newClass)
+            axiosSecure.post('/addclasses', newClass)
+            .then(data =>{
+                console.log('add item',data.data)
+            })
+
+
+        }
+    })
+};
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 {/* ------- */}
-              <div className="flex gap-4">
-              <div className="form-control w-full max-w-xs">
+                <div className="flex gap-4">
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text font-bold">Class name
+                            </span>
+                        </label>
+                        <input type="text" placeholder="Classes name" className="input input-bordered w-full " {...register("name", {required: true, maxLength: 110})} />
+                    </div>
+
+                    <div className="form-control w-full">
                     <label className="label">
-                        <span className="label-text font-bold">Class name
-                        </span>
+                        <span className="label-text font-bold">Classes Image</span>
                     </label>
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    <input type="file" {...register("image", { required: true })} className="file-input file-input-bordered w-full" />
                 </div>
-                
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text font-bold">Class Image
-                        </span>
-                    </label>
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
                 </div>
-              </div>
                 {/* ------- */}
-              <div className="flex gap-4">
-              <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text font-bold">Instructor name
-                        </span>
-                    </label>
-                    <input type="text" placeholder="Type here" readOnly className="input input-bordered w-full max-w-xs" />
+                <div className="flex gap-4 my-3">
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text font-bold">Instructor name
+                            </span>
+                        </label>
+                        <input type="text" {...register("instructorName", {required: true, maxLength: 110})} placeholder="Instructor name" readOnly className="input input-bordered w-full " defaultValue={user?.displayName} />
+                    </div>
+
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text font-bold">Instructor email
+                            </span>
+                        </label>
+                        <input type="text" {...register("email", {required: true, maxLength: 110})} placeholder="Instructor email" readOnly className="input input-bordered w-full " defaultValue={user?.email} />
+                    </div>
                 </div>
-                
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text font-bold">Instructor email
-                        </span>
-                    </label>
-                    <input type="text" placeholder="Type here" readOnly className="input input-bordered w-full max-w-xs" />
-                </div>
-              </div>
                 {/* ------- */}
-              <div className="flex gap-4">
-              <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text font-bold">Available seats
-                        </span>
-                    </label>
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                <div className="flex gap-4">
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text font-bold">Available seats
+                            </span>
+                        </label>
+                        <input type="number" {...register("availableSeats", {required: true})} placeholder="Available seats" className="input input-bordered w-full " />
+                    </div>
+
+                    <div className="form-control w-full ">
+                        <label className="label">
+                            <span className="label-text font-bold">Price
+                            </span>
+                        </label>
+                        <input type="number" {...register("price", {required: true})} placeholder="Price" className="input input-bordered w-full " />
+                    </div>
                 </div>
-                
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text font-bold">Price
-                        </span>
-                    </label>
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                </div>
-              </div>
+                <input className="btn bg-gray-400 text-white w-full mt-3" type="submit" value="Add Classes" />
             </form>
         </div>
     );
